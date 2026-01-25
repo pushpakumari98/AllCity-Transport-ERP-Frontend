b8c301b4327f5fd5624eb22dd0eead0d7c87ad96
pushpakumari98 <pushpa20052002kumari@gmail.com>
2026-01-25 08:18:01 +0530

    jb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://api.allcity-transport.com'
+  apiUrl: 'https://api.allcity-transport.com/api/vehicle-purchases'
+
 };
